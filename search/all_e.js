fa35d046077d30a7c0cb2d1de0ef32d501f05d1b
KillingJacky <jacky.shaoxg@gmail.com>
2018-04-09 08:26:16 +0000

    update by robot

--- search/all_e.js
+++ search/all_e.js
@@ -6,6 +6,7 @@ var searchData=
   ['setchainstate',['SetChainState',['../classrespeaker_1_1ReSpeaker.html#a4164daf86f69a5396b8b85daf75f7723',1,'respeaker::ReSpeaker']]],
   ['setdoaecwhenlisten',['SetDoAecWhenListen',['../classrespeaker_1_1VepDoaKwsNode.html#a7a16870d2c524e3e1b8f3f1af00c9bc6',1,'respeaker::VepDoaKwsNode']]],
   ['setloglevel',['SetLogLevel',['../classrespeaker_1_1ReSpeaker.html#a79f5b9622b7603bb97f688d94de0bcd3',1,'respeaker::ReSpeaker']]],
+  ['setthreadpriority',['SetThreadPriority',['../classrespeaker_1_1BaseNode.html#a9a1f5f6f0655912b8eca7f2d285a3dcd',1,'respeaker::BaseNode']]],
   ['settriggerpostconfirmthresholdtime',['SetTriggerPostConfirmThresholdTime',['../classrespeaker_1_1VepDoaKwsNode.html#a4bd2a35bdff6ea8ebcde5709fd463420',1,'respeaker::VepDoaKwsNode']]],
   ['start',['Start',['../classrespeaker_1_1ReSpeaker.html#ad751960137fb5453e966e96c7fb0cdd0',1,'respeaker::ReSpeaker']]],
   ['state',['state',['../structrespeaker_1_1ChainSharedData.html#ab55958f68aa075bf383b50cf2b32c9f1',1,'respeaker::ChainSharedData']]],
