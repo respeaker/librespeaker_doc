6eed83606c53d461c6df703b2cbc220c9cdfcf50
KillingJacky <jacky.shaoxg@gmail.com>
2019-01-10 17:26:40 +0800

    update by robot

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['aloopoutputnode',['AloopOutputNode',['../classrespeaker_1_1AloopOutputNode.html',1,'respeaker']]],
   ['alsacollectornode',['AlsaCollectorNode',['../classrespeaker_1_1AlsaCollectorNode.html',1,'respeaker']]]
 ];
--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['aloop_5foutput_5fnode',['ALOOP_OUTPUT_NODE',['../namespacerespeaker.html#a1f207751fb0d84d3e943e8634c33b7d1a6ccba8f7466d63ad331e22da99ae0019',1,'respeaker']]],
   ['alsa_5fcollector_5fnode',['ALSA_COLLECTOR_NODE',['../namespacerespeaker.html#a1f207751fb0d84d3e943e8634c33b7d1a2fa28f4eed18abae9462855586f0474c',1,'respeaker']]]
 ];
